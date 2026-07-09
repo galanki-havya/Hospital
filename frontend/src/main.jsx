f1a1fd4340c1b0dfdd22de7b87b84d9366df263f
galanki-havya <galankihavya@gmail.com>
2026-07-09 17:48:34 +0530

    Update package.json and main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
@@ -31,7 +30,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Toaster position="top-right" toastOptions={{ duration: 3500, style: { fontSize: '14px' } }} />
           </PlatformAuthProvider>
         </AuthProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
